d7ca29b042b840a5f99488eff3ca11552d4359e9
sandeep.chavan <sandeep@maiara.in>
2021-09-29 19:38:00 +0530

    first commit

---
+++ UserInfo/src/main/java/com/example/demo/DTO/UserDTO.java
@@ -0,0 +1,84 @@
+package com.example.demo.DTO;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class UserDTO {
+
+		
+	private Long id;
+			
+	private String firstName;
+	
+	private String lastName;
+	
+	private String city;
+	
+	private String emailID;
+
+	private String mobileNumber;
+
+	private Date dob;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getEmailID() {
+		return emailID;
+	}
+
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	
+}
